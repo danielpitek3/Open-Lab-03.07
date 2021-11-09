17d79988877a30a9ec3b7b2b2008626ac106ea2e
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-09 12:36:06 +0100

    vyriesene

--- Open-Lab-03.07/Divider.cs
+++ Open-Lab-03.07/Divider.cs
@@ -6,7 +6,19 @@ namespace Open_Lab_03._07
     {
         public bool EqualSlices(int total, int people, int each)
         {
-            throw new NotImplementedException();
+            int t = people * each;
+            if (t > total)
+            {
+                return false;
+            }
+            else if (t == total)
+            {
+                return true;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
